2dc915bb7ffc32f3a840e5e0482897e99afa136e
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2022-04-20 11:52:31 +0100

    Feat: Added logout function in default component and store file.

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -103,6 +103,7 @@ import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuIt
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
 import { useStore } from 'vuex'
 import { computed } from 'vue'
+import { useRouter } from 'vue-router'
 
 const navigation = [
   { name: 'Dashboard', to: {name: 'Dashboard'}},
@@ -124,9 +125,19 @@ export default {
   },
   setup() {
     const store = useStore();
+    const router = useRouter();
+
+    function logout() {
+      store.commit('logout');
+      router.push({
+        name: 'Login'
+      });
+    }
+
     return {
       user: computed(() => store.state.user.data),
       navigation,
+      logout
     }
   },
 }
